8e9d0bfd6d0cb46678cc494a3a5d46960817066f
Martian <Martian@MartiandeMacBook-Pro.local>
2011-08-05 12:23:25 +0800

    Initial Commit

---
+++ DynamicLyrics/Lyrics.h
@@ -0,0 +1,14 @@
+//
+//  Lyrics.h
+//  DynamicLyrics
+//
+//  Created by Zheng Zhu on 11-8-5.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Lyrics : NSDocument {
+}
+
+@end
---
+++ DynamicLyrics/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  DynamicLyrics
+//
+//  Created by Zheng Zhu on 11-8-5.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
